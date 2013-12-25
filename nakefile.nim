d147b7d5e5cd5a453e8e46b843bbf109d0e9f522
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2013-12-25 16:48:40 +0100

    Uses direshell to report errors back.

--- nakefile.nim
+++ nakefile.nim
@@ -4,13 +4,13 @@ const
   exe_name = "normalize_filenames"
 
 task "local_install", "Copies " & exe_name & " to your ~/bin directory":
-  if shell("nimrod c", exe_name):
-    let dest = getHomeDir() / "bin" / exe_name
-    copyFile(exe_name, dest)
-    dest.setFilePermissions(exe_name.getFilePermissions)
+  direshell("nimrod c", exe_name)
+  let dest = getHomeDir() / "bin" / exe_name
+  copyFile(exe_name, dest)
+  dest.setFilePermissions(exe_name.getFilePermissions)
 
 task "test", "Performs a few renaming tests":
-  if shell("nimrod c", exe_name):
-    shell("./" & exe_name & " \"_uc368_/[CAM] 130708 _uc368_ub2c8_ud790 Goodbye To Romance - KBS _ub77c_ub514_uc624 __uc774_ubb34_uc1a1, _uc784_uc218_ubbfc_uc758 _ud76c_ub9dd_uac00_uc694_ _uacf5_uac1c_ubc29_uc1a1_hd.mp4\"")
-    shell("./" & exe_name & " \"_uc368_/[CAM] 130708 _uc368_ub2c8_ud790 _ub9cc_uc778_uc758 _uc5f0_uc778 - KBS _ub77c_ub514_uc624 __uc774_ubb34_uc1a1, _uc784_uc218_ubbfc_uc758 _ud76c_ub9dd_uac00_uc694_ _uacf5_uac1c_ubc29_uc1a1_hd.mp4\"")
-    shell("./" & exe_name & " \"[CAM] 130708 _uc368_ub2c8_ud790 Goodbye To Romance - KBS _ub77c_ub514_uc624 __uc774_ubb34_uc1a1, _uc784_uc218_ubbfc_uc758 _ud76c_ub9dd_uac00_uc694_ _uacf5_uac1c_ubc29_uc1a1_hd.mp4\" \"[CAM] 130708 _uc368_ub2c8_ud790 _ub9cc_uc778_uc758 _uc5f0_uc778 - KBS _ub77c_ub514_uc624 __uc774_ubb34_uc1a1, _uc784_uc218_ubbfc_uc758 _ud76c_ub9dd_uac00_uc694_ _uacf5_uac1c_ubc29_uc1a1_hd.mp4\"")
+  direshell("nimrod c", exe_name)
+  direshell("./" & exe_name & " \"_uc368_/[CAM] 130708 _uc368_ub2c8_ud790 Goodbye To Romance - KBS _ub77c_ub514_uc624 __uc774_ubb34_uc1a1, _uc784_uc218_ubbfc_uc758 _ud76c_ub9dd_uac00_uc694_ _uacf5_uac1c_ubc29_uc1a1_hd.mp4\"")
+  direshell("./" & exe_name & " \"_uc368_/[CAM] 130708 _uc368_ub2c8_ud790 _ub9cc_uc778_uc758 _uc5f0_uc778 - KBS _ub77c_ub514_uc624 __uc774_ubb34_uc1a1, _uc784_uc218_ubbfc_uc758 _ud76c_ub9dd_uac00_uc694_ _uacf5_uac1c_ubc29_uc1a1_hd.mp4\"")
+  direshell("./" & exe_name & " \"[CAM] 130708 _uc368_ub2c8_ud790 Goodbye To Romance - KBS _ub77c_ub514_uc624 __uc774_ubb34_uc1a1, _uc784_uc218_ubbfc_uc758 _ud76c_ub9dd_uac00_uc694_ _uacf5_uac1c_ubc29_uc1a1_hd.mp4\" \"[CAM] 130708 _uc368_ub2c8_ud790 _ub9cc_uc778_uc758 _uc5f0_uc778 - KBS _ub77c_ub514_uc624 __uc774_ubb34_uc1a1, _uc784_uc218_ubbfc_uc758 _ud76c_ub9dd_uac00_uc694_ _uacf5_uac1c_ubc29_uc1a1_hd.mp4\"")
